0251cab9eddd783132fda8ee26165325b4ccff9a
MichelangeloCaltagirone <michelangelocaltagirone@gmail.com>
2024-10-10 17:55:35 +0200

    Push for debug the Bootstrap Modal (not correct behaviour)

--- index.php
+++ index.php
@@ -20,10 +20,12 @@ var_dump($padrino-> describeSelf());
 var_dump($bladeRunner-> describeSelf());
 var_dump($sevenSeal-> describeSelf());
 var_dump($spaceOdyssey-> describeSelf());
+var_dump($padrino);
+
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="it">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -53,22 +55,24 @@ var_dump($spaceOdyssey-> describeSelf());
                     </p>
 
                     <!-- Button trigger modal -->
-                    <button type="button" class="btn btn-primary" data-toggle="modal" data-target="exampleModal">
+                    <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#myModal">
                     View Cast
                     </button>
 
                     <!-- Modal -->
-                    <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                    <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
                     <div class="modal-dialog" role="document">
                         <div class="modal-content">
                         <div class="modal-header">
-                            <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                            <h5 class="modal-title" id="exampleModalLabel">Cast</h5>
                             <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                             <span aria-hidden="true">&times;</span>
                             </button>
                         </div>
                         <div class="modal-body">
-                            ...
+                           <?php foreach($movie->cast as $actor) { ?>
+                                <span class="fw-bold"><?= $actor ?></span>
+                            <? } ?>
                         </div>
                         <div class="modal-footer">
                             <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
@@ -86,6 +90,7 @@ var_dump($spaceOdyssey-> describeSelf());
 
     </div>
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+    <!-- Bootstrap JS -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 </html>
